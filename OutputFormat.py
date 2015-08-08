936b11433d7a20046dbfd34503c96ee3e5e03afb
nz123 <nogazu@gmail.com>
2015-08-08 11:42:12 +0300

    class that represents words with their tags

---
+++ OutputFormat.py
@@ -0,0 +1,22 @@
+from Tokenz import punctuation_not_for_regex
+
+class OutputFormat:
+
+
+    def __init__(self,word,tag="regular"):
+        self.tag_translation = {"regular" : "O", "person" : "PERSON", "place" : "LOCATION", "company" : "ORGANIZATION" , "ne" : "NE"}
+        self.word = word
+        self.tag = tag
+
+    def get_output(self):
+        if self.word == ".\r\n" or self.word == ".\n":
+            return ".\r\n"
+        if self.word == " " or self.word == "":
+            return ""
+        if self.word in punctuation_not_for_regex:
+            return self.word
+        return self.word + "/" + self.tag_translation[self.tag]
+
+
+    def update_tag(self,tag):
+        self.tag = tag
