2b6aa14adfd6ade066ce56e299d708852c487365
nz123 <nogazu@gmail.com>
2015-08-08 12:05:37 +0300

    temporary main

--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
-
+import sys
+from Process import *
+from Classification import *
+from WordOccurrenceClassifier import *
+from DuckduckgoSearch import *
+from FacebookSearch import *
+from DictionarySearch import *
 
 DICTIONARY_PATH = r"C:\Users\Nini\Desktop\limudim\nlproj\git\Additional\dictionary.txt"
 
 
+
+def __main__(argv):
+
+    process_corpus("eng1",1,r"C:\Users\Nini\Desktop\limudim\nlproj\git\Additional\tiny_corpus.txt")
+
+    return 1
+
+
+if __name__ == "__main__":
+    __main__(sys.argv[1:])
 
