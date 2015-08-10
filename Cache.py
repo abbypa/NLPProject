3d025d15b567115c1c2f50bd3703b6e3f75283b9
nz123 <nogazu@gmail.com>
2015-08-10 15:36:51 +0300

    general class for keeping a cache of known results

---
+++ Cache.py
@@ -0,0 +1,53 @@
+from Tokenz import char_encode
+import codecs
+from ClassificationCommon import *
+ 
+
+class Cache:
+
+    def __init__(self, cache_path,lang):
+        self.cache_path = cache_path
+        self.lang = lang
+        self.cache = dict()
+
+    def load(self):
+        cache_file = codecs.open(self.cache_path, "r", encoding=char_encode[self.lang])
+        for l in cache_file:
+            tmp = l.split(",")
+            matches = dict()
+            for i in range(len(categories)):
+                matches[categories[i]] = tmp[i+1]
+            self.cache[tmp[0]] = ClassificationResult(tmp[0], matches)
+        cache_file.close()
+
+    def save(self):
+        cache_file = codecs.open(self.cache_path, "w", encoding=char_encode[self.lang])
+        for term in self.cache.keys():
+            tmp = [term]
+            tmp.append(self.cache[term].Matches["person"])
+            tmp.append(self.cache[term].Matches["company"])
+            tmp.append(self.cache[term].Matches["place"])
+            tmp.append(self.cache[term].Matches["regular"])
+            for i in range(len(tmp)):
+                tmp[i] = str(tmp[i])
+            cache_file.write(",".join(tmp) + ",\r\n")
+        cache_file.close()
+
+    def search_cache(self,term):
+        if term in self.cache.keys():
+            return self.cache[term]
+        else:
+            return None
+
+    def update_cache(self,term,matches):
+        self.cache[term] = matches
+
+    def update_cache_from_list(self,term,cnt):
+        matches = dict()
+        for i in range(len(categories)):
+            matches[categories[i]] = cnt[i]
+        self.cache[term] = ClassificationResult(term, matches)
+
+        
+        
+            
